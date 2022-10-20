8dbabdac9c384771a152193b41194a536d3b3e59
amirulasri <amirulasrix@gmail.com>
2022-10-20 23:47:15 +0800

    Fix link nav not working

--- staffmanager/doctors.php
+++ staffmanager/doctors.php
@@ -43,10 +43,10 @@ if (isset($_SESSION['adminid'])) {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Staffs</a>
+                        <a class="nav-link" aria-current="page" href="index">Staffs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="doctors">Doctors</a>
+                        <a class="nav-link active" href="#">Doctors</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="logout">Logout</a>
